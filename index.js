bcf41971f464e9e1950377529a3144a26cabf942
Ian Walter <public@iankwalter.com>
2019-04-18 21:46:02 -0400

    Refactoring for bff v3

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const { Print } = require('@ianwalter/print')
 
+const hasBslKey = cap => cap.hasOwnProperty('browserstack.local')
+
+function shouldStartBsl (capabilities) {
+  if (Array.isArray(capabilities)) {
+    return capabilities.some(hasBslKey)
+  } else {
+    return hasBslKey(capabilities)
+  }
+}
+function toBrowserTest (test) {
+  return capability => {
+    //
+    capability.name = test.name
+
+    //
+    let name = `${test.name} in ${capability.browserName}`
+    if (capability.browserVersion) {
+      name += ` ${capability.browserVersion}`
+    }
+    if (capability.platformName) {
+      name += ` on ${capability.platformName}`
+    }
+    return { ...test, name, capability }
+  }
+}
+function toBrowserTests (capabilties) {
+  return (acc, test) => acc.concat(capabilties.map(toBrowserTest(test)))
+}
+
 module.exports = async function bffWebdriver (hook, context) {
-  const print = new Print({ level: context.config.logLevel })
+  const print = new Print({ level: context.logLevel })
   print.debug(`Hook ${hook}:`, context)
 
-  if (hook === 'before') {
-    if (context.config.webdriverBackend === 'browserstack') {
-      print.debug('Starting BrowserStadk Local')
-      const bsl = require('@ianwalter/bsl')
-      context.browserstackLocal = await bsl.start()
-    } else if (context.config.webdriverBackend === 'standalone') {
+  const multipleCapabilities = Array.isArray(context.webdriver.capabilities)
+  if (hook === 'registration' && multipleCapabilities) {
+    const registrationContext = context.registrationContext
+    registrationContext.tests = registrationContext.tests.reduce(
+      toBrowserTests(context.webdriver.capabilities),
+      []
+    )
+  } else if (hook === 'before') {
+    if (process.env.SELENIUM_STANDALONE) {
       print.debug('Starting Selenium Standalone')
       return new Promise((resolve, reject) => {
         const seleniumStandalone = require('selenium-standalone')
@@ -26,17 +58,25 @@ module.exports = async function bffWebdriver (hook, context) {
           }
         })
       })
+    } else if (shouldStartBsl(context.webdriver.capabilities)) {
+      print.debug('Starting BrowserStadk Local')
+      const bsl = require('@ianwalter/bsl')
+      context.browserstackLocal = await bsl.start()
     }
   } else if (hook === 'beforeEach') {
     print.debug('Creating WebdriverIO browser instance')
-    // Set up the browser instance and add it to the context.
-    // const { remote } = require('webdriverio')
-    // const config = {}
-    // context.browser = await remote(config)
+
+    // Set up the browser instance and add it to the test context.
+    const { remote } = require('webdriverio')
+    context.testContext.browser = await remote({
+      ...context.webdriver,
+      capabilities: context.testContext.test.capability
+    })
   } else if (hook === 'adterEach') {
     print.debug('Terminating WebdriverIO browser instance')
+
     // Tell Selenium to delete the browser session once the test is over.
-    // await context.browser.deleteSession()
+    await context.testContext.browser.deleteSession()
   } else if (hook === 'after') {
     if (context.seleniumStandalone) {
       print.debug('Stopping Selenium Standalone')
