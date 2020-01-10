03d9819f132c4b161dfa8dc76d5f0a1a2d193287
Ian Walter <public@iankwalter.com>
2020-01-10 14:36:17 -0500

    Setting hostname

--- config.js
+++ config.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 module.exports = {
   before (context) {
     const standalone = process.env.SELENIUM_STANDALONE
+    const hostname = process.env.SELENIUM_HUB_HOST
     if (standalone) {
       context.webdriver.standalone = standalone
+    } else if (hostname) {
+      context.webdriver.hostname = hostname
     } else {
       context.webdriver.browserstack = true
     }
